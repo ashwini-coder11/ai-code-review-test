e0e5b7f5cd20317af9f8db560a7295138d9af874
Ashwini-coder11 <ashwinikumar88229@gmail.com>
2026-08-20 12:47:25 +0530

    Add clean no-issue test case

---
+++ clean/clean_example.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    return a + b
+
+def format_message(name):
+    return f"Hello, {name}!"
+
+if __name__ == '__main__':
+    print(add_numbers(2, 3))
+    print(format_message('world'))
